a0813904d6fc5bc720828cd404c97e3b97f49e4f
vincent <vsordo@gmail.com>
2018-04-03 15:11:18 -0700

    Fixed MONGODB_URI typo

--- db/mongoose.js
+++ db/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // set mongoose to use Javascript Promise, since ES6 Promises are supported in JS
 mongoose.Promise = global.Promise;
 // connect to mongo db
-let dbUrl = process.ENV.MONGODB_URI || 'mongodb://localhost:27017/todo_app';
+let dbUrl = process.env.MONGODB_URI || 'mongodb://localhost:27017/todo_app';
 mongoose.connect(dbUrl);
 
 module.exports = {mongoose};
