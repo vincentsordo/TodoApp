afbb69d93ca20bd027c0bb34704d46d416c6742a
vincent <vsordo@gmail.com>
2018-04-05 15:50:50 -0700

    Refactored config loading to json file

--- config/config.js
+++ config/config.js
@@ -1,9 +1,10 @@
 let env = process.env.NODE_ENV || 'development';
 
-if (env === 'development') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/todo_app';
-} else if (env === 'test') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/todo_app_test';
+if (env === 'development' || env === 'test') {
+  let config = require('./config.json');
+  let environmentConfig = config[env];
+
+  Object.keys(environmentConfig).forEach((key) => {
+    process.env[key] = environmentConfig[key];
+  });
 }
--- test/seed/seed.js
+++ test/seed/seed.js
@@ -13,7 +13,7 @@ const users = [
     tokens: [
       {
         access: 'auth',
-        token: jwt.sign({_id: userOneId.toHexString(), access: 'auth'}, 'abc123').toString()
+        token: jwt.sign({_id: userOneId.toHexString(), access: 'auth'}, process.env.JWT_SECRET).toString()
       }]
   },
   {
@@ -23,7 +23,7 @@ const users = [
     tokens: [
       {
         access: 'auth',
-        token: jwt.sign({_id: userTwoId.toHexString(), access: 'auth'}, 'abc123').toString()
+        token: jwt.sign({_id: userTwoId.toHexString(), access: 'auth'}, process.env.JWT_SECRET).toString()
       }]
   }
 ];
