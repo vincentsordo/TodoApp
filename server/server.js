a3eb85d28e016915d075e46b4622ed4305f5fbf2
vincent <vsordo@gmail.com>
2018-04-05 14:09:19 -0700

    Added tests for POST /user/login route

--- server/server.js
+++ server/server.js
@@ -114,6 +114,16 @@ app.get('/user/me', authenticate, (req, res) => {
   res.send(req.user);
 });
 
+app.post('/user/login', (req, res) => {
+  let body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => res.status(400).send());
+});
+
 app.get('/user', (req, res) => {
   User.find().then((users) => {
     res.send({
